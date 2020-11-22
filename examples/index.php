b64c2e74e977e9e3a36c38ce8e13742341315d2c
Amir Hossein Baghaie <amirbagh75@users.noreply.github.com>
2020-11-22 09:20:10 +0000

    Apply fixes from StyleCI

--- examples/index.php
+++ examples/index.php
@@ -5,13 +5,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Amirbagh75\Chalqoz\Chalqoz;
 
 $newText = Chalqoz::convertEnglishNumbersToPersian('123456789');
-echo($newText."\n");
+echo($newText . "\n");
 
 $newText = Chalqoz::convertPersianNumbersToEnglish('۱۲۳۴۵۶۶۹۸۷۴');
-echo($newText."\n");
+echo($newText . "\n");
 
 $newText = Chalqoz::convertPersianNumbersToEnglish('امیرحسین بقایی ۲۴ سال دارد.');
-echo($newText."\n");
+echo($newText . "\n");
 
 $newText = Chalqoz::convertEnglishNumbersToPersian('امیرحسین بقایی 24 سال دارد.');
-echo($newText."\n");
+echo($newText . "\n");
