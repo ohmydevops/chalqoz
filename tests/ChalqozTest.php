fa9cdefa414e9d75a5164b8e24b311508ad24d87
Amir Hossein Baghaie <amirbagh75@gmail.com>
2020-11-25 19:19:31 +0330

    Add unit test (#2)
    * Add unit tests
    
    * Add github Action for unit tests

---
+++ tests/ChalqozTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Amirbagh75\Chalqoz;
+
+use PHPUnit\Framework\TestCase;
+
+final class ChalqozTest extends TestCase
+{
+
+    public function testConvertPersianNumbersToEnglish()
+    {
+        $this->assertEquals(
+            'امیرحسین 24 سال دارد.',
+            Chalqoz::convertPersianNumbersToEnglish('امیرحسین ۲۴ سال دارد.')
+        );
+
+        $this->assertEquals(
+            '123456789',
+            Chalqoz::convertPersianNumbersToEnglish('۱۲۳۴۵۶۷۸۹')
+        );
+    }
+
+    public function testConvertEnglishNumbersToPersian()
+    {
+        $this->assertEquals(
+            'امیرحسین ۲۴ سال دارد.',
+            Chalqoz::convertEnglishNumbersToPersian('امیرحسین 24 سال دارد.')
+        );
+
+        $this->assertEquals(
+            '۱۲۳۴۵٦۷۸۹',
+            Chalqoz::convertEnglishNumbersToPersian('123456789')
+        );
+    }
+}
